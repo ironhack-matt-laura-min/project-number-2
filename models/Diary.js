f9279a539c7b5b05751b4320fc7ec426bc8ebf19
Laura PR <laurapr.022@gmail.com>
2019-02-11 19:45:16 +0100

    basic routes done

---
+++ models/Diary.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const diarySchema = new Schema({
+  title: { type: String, required: true },
+  description: String,
+  timeSpent: Number,
+  difficulty: { type: String, enum: ["1", "2", "3", "4", "5"] },
+  sourceType: { type: String, enum: ["video", "book", "article", "course", "other"] },
+  sourceTitle: { type: String, required: true },
+  sourceLink: { type: String }
+}, {
+    timestamps: {
+      createdAt: 'created_at',
+      updatedAt: 'updated_at'
+    }
+  });
+
+const Diary = mongoose.model('Diary', diarySchema);
+module.exports = Diary;
+
+
