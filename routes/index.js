9bc17d9e97f336c2a9bb7aaeb84855d7f9d3cbbb
Ang Min Sheng <angminsheng@Angs-MacBook-Pro.local>
2019-02-12 15:13:34 +0100

    add diary done

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,9 @@ router.get('/', (req, res, next) => {
   res.render('index');
 });
 
-router.post('/add-post', (req, res, next) => {
+router.post('/add-diary', (req, res, next) => {
+  const user = req.user
+
   const {
     category,
     description,
@@ -17,7 +19,11 @@ router.post('/add-post', (req, res, next) => {
     sourceTitle,
     sourceLink
   } = req.body;
-  Diary.create({
+
+  console.log(req.body)
+
+  const newDiary = new Diary({
+    _owner: user,
     category,
     description,
     timeSpent,
@@ -26,8 +32,10 @@ router.post('/add-post', (req, res, next) => {
     sourceTitle,
     sourceLink
   })
+
+  newDiary.save()
     .then(() => {
-      res.redirect('/');
+      res.redirect('/home');
     })
     .catch(err => next(err));
 });
