faf685c0d71547a316808685d24b92da9fc78d10
Ang Min Sheng <angminsheng@Angs-MacBook-Pro.local>
2019-02-12 14:33:45 +0100

    index2

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.get('/', (req, res, next) => {
   res.render('index');
 });
 
-router.post('/', (req, res, next) => {
+router.post('/add-post', (req, res, next) => {
   const {
     category,
     description,
