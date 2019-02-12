d2096508b6bc6d76310f37511599d2a469152a58
Ang Min Sheng <angminsheng@Angs-MacBook-Pro.local>
2019-02-12 13:13:32 +0100

    modify model

--- models/Diary.js
+++ models/Diary.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const diarySchema = new Schema({
+  _owner: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  },
   category: { type: String, required: true },
   description: String,
   timeSpent: Number,
