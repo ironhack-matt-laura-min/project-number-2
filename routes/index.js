6cb46330091242b9a51bee945e3f39ffbd87e7d8
Laura PR <laurapr.022@gmail.com>
2019-02-12 15:32:34 +0100

    pulling new version

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,8 @@ const Diary = require('../models/Diary');
 const router = express.Router();
 const passport = require("passport");
 const ensureLogin = require("connect-ensure-login");
-const { checkConnected } = require('../configs/middlewares')
-
-
-
+const { checkConnected } = require('../configs/middlewares');
+const User = require("../models/User")
 
 
 /* GET home page */
@@ -14,40 +12,12 @@ router.get('/', (req, res, next) => {
   res.render('index');
 });
 
-<<<<<<< HEAD
 /* GET Abous us page */
-=======
-router.post('/add-post', (req, res, next) => {
-  const {
-    category,
-    description,
-    timeSpent,
-    difficulty,
-    sourceType,
-    sourceTitle,
-    sourceLink
-  } = req.body;
-  Diary.create({
-    category,
-    description,
-    timeSpent,
-    difficulty,
-    sourceType,
-    sourceTitle,
-    sourceLink
-  })
-    .then(() => {
-      res.redirect('/');
-    })
-    .catch(err => next(err));
-});
->>>>>>> faf685c0d71547a316808685d24b92da9fc78d10
 
 router.get('/about', (req, res, next) => {
   res.render('about');
 });
 
-<<<<<<< HEAD
 //GET home page - after user signed in 
 router.get("/home", checkConnected, (req, res, next) => {
   res.render('index2')
@@ -59,15 +29,6 @@ router.get("/home", checkConnected, (req, res, next) => {
 router.get("/home/profile", checkConnected, (req, res, next) => {
   res.render('Profile')
 })
-=======
-router.get('/home', (req, res, next) => {
-  res.render('index2');
-});
-
-router.get('/home/profile', (req, res, next) => {
-  res.render('Profile');
-});
->>>>>>> faf685c0d71547a316808685d24b92da9fc78d10
 
 
 module.exports = router;
