1bc0000ae198b5d05a3a876c87c72e16b06c3b2a
Matt Harrison <fittocode@gmail.com>
2019-02-12 13:16:57 +0100

    added router.post on index.js

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Diary = require('../models/Diary');
 const router = express.Router();
 
 /* GET home page */
@@ -6,16 +7,41 @@ router.get('/', (req, res, next) => {
   res.render('index');
 });
 
+router.post('/', (req, res, next) => {
+  const {
+    category,
+    description,
+    timeSpent,
+    difficulty,
+    sourceType,
+    sourceTitle,
+    sourceLink
+  } = req.body;
+  Diary.create({
+    category,
+    description,
+    timeSpent,
+    difficulty,
+    sourceType,
+    sourceTitle,
+    sourceLink
+  })
+    .then(() => {
+      res.redirect('/');
+    })
+    .catch(err => next(err));
+});
+
 router.get('/about', (req, res, next) => {
-  res.render('about')
-})
+  res.render('about');
+});
 
-router.get("/home", (req, res, next) => {
-  res.render('index2')
-})
+router.get('/home', (req, res, next) => {
+  res.render('index2');
+});
 
-router.get("/home/profile", (req, res, next) => {
-  res.render('Profile')
-})
+router.get('/home/profile', (req, res, next) => {
+  res.render('Profile');
+});
 
 module.exports = router;
